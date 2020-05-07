30e64c90acf294f3b2adfed9ce52872a645a75c1
Seth Bashford <seth.bashford@boystown.org>
2020-05-07 10:27:30 -0500

    adding to main impl

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
+#include "threeAS-task.hpp"
 #include "ofMain.h"
 #include "ofApp.h"
 
+namespace three_as {
+namespace {
+class OpenFrameworks : public Framework {
+  public:
+    void eventLoop() override { ofRunApp(new ofApp()); }
+};
+
+void main() {
+    OpenFrameworks framework;
+    runTask(framework);
+}
+}
+}
+
 //========================================================================
-int main( ){
-	ofSetupOpenGL(1920,1080,OF_WINDOW);			// <-------- setup the GL context
+int main() {
+    ofSetupOpenGL(1920, 1080, OF_WINDOW); // <-------- setup the GL context
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp(new ofApp());
-
+    // this kicks off the running of my app
+    // can be OF_WINDOW or OF_FULLSCREEN
+    // pass in width and height too:
+    three_as::main();
 }
